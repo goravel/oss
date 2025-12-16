06e0e447262e8fdb0348bbc9cd93b5f5137b48ba
Bowen <hwbrzzl@gmail.com>
2025-12-16 17:34:34 +0800

    optimize setup

--- setup/setup.go
+++ setup/setup.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+	setup := packages.Setup(os.Args)
 	config := `map[string]any{
         "driver": "custom",
         "key":      config.Env("ALIYUN_ACCESS_KEY_ID"),
@@ -25,56 +26,53 @@ func main() {
 
 	appConfigPath := path.Config("app.go")
 	filesystemsConfigPath := path.Config("filesystems.go")
-	modulePath := packages.GetModulePath()
+	moduleImport := setup.Paths().Module().Import()
 	ossServiceProvider := "&oss.ServiceProvider{}"
 	filesystemContract := "github.com/goravel/framework/contracts/filesystem"
 	ossFacades := "github.com/goravel/oss/facades"
 	filesystemsDisksConfig := match.Config("filesystems.disks")
 	filesystemsConfig := match.Config("filesystems")
 
-	packages.Setup(os.Args).
-		Install(
-			// Add oss service provider to app.go if not using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return !env.IsBootstrapSetup()
-			}, modify.GoFile(appConfigPath).
-				Find(match.Imports()).Modify(modify.AddImport(modulePath)).
-				Find(match.Providers()).Modify(modify.Register(ossServiceProvider))),
+	setup.Install(
+		// Add oss service provider to app.go if not using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return !env.IsBootstrapSetup()
+		}, modify.GoFile(appConfigPath).
+			Find(match.Imports()).Modify(modify.AddImport(moduleImport)).
+			Find(match.Providers()).Modify(modify.Register(ossServiceProvider))),
 
-			// Add oss service provider to providers.go if using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return env.IsBootstrapSetup()
-			}, modify.AddProviderApply(modulePath, ossServiceProvider)),
+		// Add oss service provider to providers.go if using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return env.IsBootstrapSetup()
+		}, modify.AddProviderApply(moduleImport, ossServiceProvider)),
 
-			// Add oss disk to filesystems.go
-			modify.GoFile(filesystemsConfigPath).Find(match.Imports()).Modify(
-				modify.AddImport(filesystemContract),
-				modify.AddImport(ossFacades, "ossfacades"),
-			).
-				Find(filesystemsDisksConfig).Modify(modify.AddConfig("oss", config)).
-				Find(filesystemsConfig).Modify(modify.AddConfig("default", `"oss"`)),
+		// Add oss disk to filesystems.go
+		modify.GoFile(filesystemsConfigPath).Find(match.Imports()).Modify(
+			modify.AddImport(filesystemContract),
+			modify.AddImport(ossFacades, "ossfacades"),
 		).
-		Uninstall(
-			// Remove oss disk from filesystems.go
-			modify.GoFile(filesystemsConfigPath).
-				Find(filesystemsConfig).Modify(modify.AddConfig("default", `"local"`)).
-				Find(filesystemsDisksConfig).Modify(modify.RemoveConfig("oss")).
-				Find(match.Imports()).Modify(
-				modify.RemoveImport(filesystemContract),
-				modify.RemoveImport(ossFacades, "ossfacades"),
-			),
+			Find(filesystemsDisksConfig).Modify(modify.AddConfig("oss", config)).
+			Find(filesystemsConfig).Modify(modify.AddConfig("default", `"oss"`)),
+	).Uninstall(
+		// Remove oss disk from filesystems.go
+		modify.GoFile(filesystemsConfigPath).
+			Find(filesystemsConfig).Modify(modify.AddConfig("default", `"local"`)).
+			Find(filesystemsDisksConfig).Modify(modify.RemoveConfig("oss")).
+			Find(match.Imports()).Modify(
+			modify.RemoveImport(filesystemContract),
+			modify.RemoveImport(ossFacades, "ossfacades"),
+		),
 
-			// Remove oss service provider from app.go if not using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return !env.IsBootstrapSetup()
-			}, modify.GoFile(appConfigPath).
-				Find(match.Providers()).Modify(modify.Unregister(ossServiceProvider)).
-				Find(match.Imports()).Modify(modify.RemoveImport(modulePath))),
+		// Remove oss service provider from app.go if not using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return !env.IsBootstrapSetup()
+		}, modify.GoFile(appConfigPath).
+			Find(match.Providers()).Modify(modify.Unregister(ossServiceProvider)).
+			Find(match.Imports()).Modify(modify.RemoveImport(moduleImport))),
 
-			// Remove oss service provider from providers.go if using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return env.IsBootstrapSetup()
-			}, modify.RemoveProviderApply(modulePath, ossServiceProvider)),
-		).
-		Execute()
+		// Remove oss service provider from providers.go if using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return env.IsBootstrapSetup()
+		}, modify.RemoveProviderApply(moduleImport, ossServiceProvider)),
+	).Execute()
 }
